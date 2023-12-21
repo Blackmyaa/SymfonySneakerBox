fc7d20fc923c389cdcdb9b8a9646cd555bf08c05
Blackmyaa <ymbenzedi@icloud.com>
2023-12-21 15:18:28 +0100

    Affichage des détails d'un Produit

--- src/Controller/ProduitsController.php
+++ src/Controller/ProduitsController.php
@@ -7,7 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-#[Route('/produits', name: 'app_produits_')]
+#[Route('/produits', name: 'produits_')]
 class ProduitsController extends AbstractController
 {
     #[Route('/', name: 'index')]
